a975647eed7fc00521ba70ae89d173665ec61e82
Liran khalil <liran9800@gmail.com>
2023-12-21 21:26:03 +0200

    index , nav and header fixed url

--- includes/header.php
+++ includes/header.php
@@ -13,11 +13,11 @@
 
     <title>Blog Home - Start Bootstrap Template</title>
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="/CMS_TEMPLATE/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
-    <link href="css/blog-home.css" rel="stylesheet">
+    <link href="/CMS_TEMPLATE/css/blog-home.css" rel="stylesheet">
 
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="/CMS_TEMPLATE/css/styles.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    
