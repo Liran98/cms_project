31e228b175d4e5a38c2835d62868ba55b3e0cc0d
liran <liran9800@gmail.com>
2023-12-31 13:14:25 +0200

    need to understand more the reset page

--- reset.php
+++ reset.php
@@ -7,28 +7,15 @@
 
 <?php
 
-// if (isset($_GET['email']) && isset($_GET['token'])) {
-//     $email = $_GET['email'];
-//     $token = $_GET['token'];
+$verified = false;
 
-//     $query = "SELECT * FROM users WHERE user_email = '$email' AND token = '$token'";
-//     $res = mysqli_query($conn, $query);
+if ($_GET['token'] !== $token || $_GET['email'] !== $email) {
+    redirect('index');
+}
 
-//     if (!$res) {
-//         die(mysqli_error($conn));
-//     }
+// $email = '';
 
-//     while($row = mysqli_fetch_assoc($res)){
-//         $user = $row['user_name'];
-//     }
-
-// }else{
-//     redirect('index');
-// }
-// echo "hello $user welcome to the reset page";
-
-
-$token = '3bbce3b496d562a766e6e97ca15a06af9c431179295d0d8288ca880115716a2feec2d50dd3ab1313ec1d28ae931c7f15b67c';
+// $token = '3bbce3b496d562a766e6e97ca15a06af9c431179295d0d8288ca880115716a2feec2d50dd3ab1313ec1d28ae931c7f15b67c';
 
 $stmt = mysqli_prepare($conn, "SELECT  user_name , user_email , token FROM users WHERE token =?");
 
@@ -44,69 +31,90 @@ mysqli_stmt_close($stmt);
 
 // echo $user;
 
-// if ($_GET['token'] !== $token || $_GET['email'] !== $email) {
-//     redirect('index');
-// }
+
 
 if (isset($_POST['password']) && isset($_POST['confirmPassword'])) {
 
     if ($_POST['password'] === $_POST['confirmPassword']) {
-        echo "password match ✅";
-    }
 
+        $password = $_POST['password'];
+
+        $password =  password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+
+        if ($stmt = mysqli_prepare($conn, "UPDATE users SET token = '',user_password='{$password}' WHERE user_email = ?")) {
+
+            mysqli_stmt_bind_param($stmt, "s", $_GET['token']);
+
+            mysqli_stmt_execute($stmt);
+
+            if (mysqli_stmt_affected_rows($stmt) >= 1) {
+
+                redirect('CMS_TEMPLATE/login');
+            }
+
+            mysqli_stmt_close($stmt);
+
+            $verified = true;
+        }
+    }
 }
 ?>
 
 <div class="container">
 
+    <?php if (!$verified) : ?>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div class="text-center">
 
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="text-center">
+                                <h3><i class="fa fa-lock fa-4x"></i></h3>
+                                <h2 class="text-center">Reset Password</h2>
+                                <p>You can reset your password here.</p>
+                                <div class="panel-body">
 
 
-                            <h3><i class="fa fa-lock fa-4x"></i></h3>
-                            <h2 class="text-center">Reset Password</h2>
-                            <p>You can reset your password here.</p>
-                            <div class="panel-body">
+                                    <form id="register-form" role="form" autocomplete="off" class="form" method="post">
 
-
-                                <form id="register-form" role="form" autocomplete="off" class="form" method="post">
-
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
-                                            <input id="password" name="password" placeholder="Enter password" class="form-control" type="password">
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="glyphicon glyphicon-user color-blue"></i></span>
+                                                <input id="password" name="password" placeholder="Enter password" class="form-control" type="password">
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <span class="input-group-addon"><i class="glyphicon glyphicon-ok color-blue"></i></span>
-                                            <input id="confirmPassword" name="confirmPassword" placeholder="Confirm password" class="form-control" type="password">
+                                        <div class="form-group">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="glyphicon glyphicon-ok color-blue"></i></span>
+                                                <input id="confirmPassword" name="confirmPassword" placeholder="Confirm password" class="form-control" type="password">
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div class="form-group">
-                                        <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
-                                    </div>
+                                        <div class="form-group">
+                                            <input name="resetPassword" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                                        </div>
 
-                                    <input type="hidden" class="hide" name="token" id="token" value="">
-                                </form>
+                                        <input type="hidden" class="hide" name="token" id="token" value="">
+                                    </form>
 
-                            </div><!-- Body-->
+                                </div><!-- Body-->
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    <?php else : ?>
 
+        <?php redirect('/CMS_TEMPLATE/login');  ?>
+
+
+    <?php endif; ?>
 
     <hr>
 
